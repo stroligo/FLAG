ef058e12f1521d1062f5262c8ee89e6bf71975f5
stroligo <gabrielstroligo@gmail.com>
2024-03-23 19:22:09 +0000

    base

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,18 @@
-// Importar opções do config.js
-const options = require("./config");
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  important: true,
-  content: ["./static/*.{html,js}", "./static/**/*.{html,js}"],
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
-    extend: options.extend,
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
   },
-  plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
+  plugins: [],
 };
